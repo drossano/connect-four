5999d36bfd37ff37defcfc17fe4f7ff207bc6c71
Dean <72749793+drossano@users.noreply.github.com>
2022-08-21 11:37:20 -0400

    Add non-working code for board_array interpolation

--- lib/board.rb
+++ lib/board.rb
@@ -1,12 +1,15 @@
 class Board
 
+  attr_reader :board_array
+
   def initiaize
-    @board_array = Array.new(6, Array.new(7, " "))
+    @board_array = Array.new(6){ Array.new(7, " ")}
   end
-  def draw_board
+
+  def draw_board(array)
     ' 1 2 3 4 5 6 7 ' + "\n" +
     '┌─┬─┬─┬─┬─┬─┬─┐' + "\n" +
-    '│ │ │ │ │ │ │ │' + "\n" +
+    "│#{array[0][0]}│ │ │ │ │ │ │" + "\n" +
     '├─┼─┼─┼─┼─┼─┼─┤' + "\n" +
     '│ │ │ │ │ │ │ │' + "\n" +
     '├─┼─┼─┼─┼─┼─┼─┤' + "\n" +
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,16 +1,23 @@
 require_relative '../lib/board'
 
 describe Board do
-
+  subject(:game_board) { described_class.new }
   describe '#initialize' do
     it 'creates a 2D array of 6 arrays containing 7 sstring spaces' do
-      expect(subject.initiaize).to eq([[" ", " ", " ", " ", " ", " ", " "], [" ", " ", " ", " ", " ", " ", " "], [" ", " ", " ", " ", " ", " ", " "], [" ", " ", " ", " ", " ", " ", " "], [" ", " ", " ", " ", " ", " ", " "], [" ", " ", " ", " ", " ", " ", " "]])
+      expect(subject.initiaize).to eq([[" ", " ", " ", " ", " ", " ", " "], 
+        [" ", " ", " ", " ", " ", " ", " "], 
+        [" ", " ", " ", " ", " ", " ", " "], 
+        [" ", " ", " ", " ", " ", " ", " "], 
+        [" ", " ", " ", " ", " ", " ", " "], 
+        [" ", " ", " ", " ", " ", " ", " "]])
     end
   end
+
   describe '#draw_board' do
     context 'when the board is empty' do
       it 'shows an empty board' do
-        expect(subject.draw_board).to eq(
+        array = game_board.instance_variable_get(:@board_array)
+        expect(game_board.draw_board(array)).to eq(
         ' 1 2 3 4 5 6 7 ' + "\n" +
         '┌─┬─┬─┬─┬─┬─┬─┐' + "\n" +
         '│ │ │ │ │ │ │ │' + "\n" +
