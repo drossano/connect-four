b008f5dcdf71b567dbdda583a15e0beea4d106a8
Dean <72749793+drossano@users.noreply.github.com>
2022-08-22 17:19:15 -0400

    Confirm draw_board displays filled in spaces

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -15,7 +15,7 @@ describe Board do
 
   describe '#draw_board' do
     subject(:draw_game_board) { described_class.new }
-    context 'when the board is empty' do
+    context 'when the board array is empty' do
       it 'shows an empty board' do
         expect(draw_game_board.draw_board(draw_game_board.board_array)).to eq(
           '  1   2   3   4   5   6   7  ' + "\n" +
@@ -32,6 +32,34 @@ describe Board do
           "├───┼───┼───┼───┼───┼───┼───┤" + "\n" +
           "│   │   │   │   │   │   │   │" + "\n" +
           "└───┴───┴───┴───┴───┴───┴───┘")
+      end
+    end
+
+    context 'when the board array contains values' do
+      it 'displays those values in the correct spaces' do
+        filled_array = [[" ", " ", " ", " ", " ", " ", " "], 
+        [" ", "O", "O", " ", " ", " ", " "], 
+        [" ", " ", " ", " ", " ", " ", " "], 
+        [" ", "O", "O", " ", " ", " ", " "], 
+        [" ", "O", " ", " ", " ", " ", " "], 
+        ["O", " ", " ", " ", " ", " ", " "]]
+
+        expect(draw_game_board.draw_board(filled_array)).to eq(
+          '  1   2   3   4   5   6   7  ' + "\n" +
+          '┌───┬───┬───┬───┬───┬───┬───┐' + "\n" +
+          "│   │   │   │   │   │   │   │" + "\n" +
+          "├───┼───┼───┼───┼───┼───┼───┤" + "\n" +
+          "│   │ O │ O │   │   │   │   │" + "\n" +
+          "├───┼───┼───┼───┼───┼───┼───┤" + "\n" +
+          "│   │   │   │   │   │   │   │" + "\n" +
+          "├───┼───┼───┼───┼───┼───┼───┤" + "\n" +
+          "│   │ O │ O │   │   │   │   │" + "\n" +
+          "├───┼───┼───┼───┼───┼───┼───┤" + "\n" +
+          "│   │ O │   │   │   │   │   │" + "\n" +
+          "├───┼───┼───┼───┼───┼───┼───┤" + "\n" +
+          "│ O │   │   │   │   │   │   │" + "\n" +
+          "└───┴───┴───┴───┴───┴───┴───┘"
+        )
       end
     end
   end
