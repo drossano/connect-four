338fa2423c59bb94314eb5a2454c571263a28c0f
Dean <72749793+drossano@users.noreply.github.com>
2022-08-22 19:43:03 -0400

    Update board display to accept new board_array format

--- lib/board.rb
+++ lib/board.rb
@@ -2,7 +2,7 @@
 
 # Stores, manipulates and displays game board.
 class Board
-  attr_reader :board_array, :white_piece, :black_piece
+  attr_reader :board_array
 
   def initialize
     @board_array = Array.new(7) { Array.new(6, ' ') }
@@ -13,17 +13,17 @@ class Board
   def draw_board(array)
     '  1   2   3   4   5   6   7  ' + "\n" +
     '┌───┬───┬───┬───┬───┬───┬───┐' + "\n" +
-    "│ #{array[0][0]} │ #{array[0][1]} │ #{array[0][2]} │ #{array[0][3]} │ #{array[0][4]} │ #{array[0][5]} │ #{array[0][6]} │" + "\n" +
+    "│ #{array[0][0]} │ #{array[1][0]} │ #{array[2][0]} │ #{array[3][0]} │ #{array[4][0]} │ #{array[5][0]} │ #{array[6][0]} │" + "\n" +
     "├───┼───┼───┼───┼───┼───┼───┤" + "\n" +
-    "│ #{array[1][0]} │ #{array[1][1]} │ #{array[1][2]} │ #{array[1][3]} │ #{array[1][4]} │ #{array[1][5]} │ #{array[1][6]} │" + "\n" +
+    "│ #{array[0][1]} │ #{array[1][1]} │ #{array[2][1]} │ #{array[3][1]} │ #{array[4][1]} │ #{array[5][1]} │ #{array[6][1]} │" + "\n" +
     "├───┼───┼───┼───┼───┼───┼───┤" + "\n" +
-    "│ #{array[2][0]} │ #{array[2][1]} │ #{array[2][2]} │ #{array[2][3]} │ #{array[2][4]} │ #{array[2][5]} │ #{array[2][6]} │" + "\n" +
+    "│ #{array[0][2]} │ #{array[1][2]} │ #{array[2][2]} │ #{array[3][2]} │ #{array[4][2]} │ #{array[5][2]} │ #{array[6][2]} │" + "\n" +
     "├───┼───┼───┼───┼───┼───┼───┤" + "\n" +
-    "│ #{array[3][0]} │ #{array[3][1]} │ #{array[3][2]} │ #{array[3][3]} │ #{array[3][4]} │ #{array[3][5]} │ #{array[3][6]} │" + "\n" +
+    "│ #{array[0][3]} │ #{array[1][3]} │ #{array[2][3]} │ #{array[3][3]} │ #{array[4][3]} │ #{array[5][3]} │ #{array[6][3]} │" + "\n" +
     "├───┼───┼───┼───┼───┼───┼───┤" + "\n" +
-    "│ #{array[4][0]} │ #{array[4][1]} │ #{array[4][2]} │ #{array[4][3]} │ #{array[4][4]} │ #{array[4][5]} │ #{array[4][6]} │" + "\n" +
+    "│ #{array[0][4]} │ #{array[1][4]} │ #{array[2][4]} │ #{array[3][4]} │ #{array[4][4]} │ #{array[5][4]} │ #{array[6][4]} │" + "\n" +
     "├───┼───┼───┼───┼───┼───┼───┤" + "\n" +
-    "│ #{array[5][0]} │ #{array[5][1]} │ #{array[5][2]} │ #{array[5][3]} │ #{array[5][4]} │ #{array[5][5]} │ #{array[5][6]} │" + "\n" +
+    "│ #{array[0][5]} │ #{array[1][5]} │ #{array[2][5]} │ #{array[3][5]} │ #{array[4][5]} │ #{array[5][5]} │ #{array[6][5]} │" + "\n" +
     '└───┴───┴───┴───┴───┴───┴───┘'
   end
 
