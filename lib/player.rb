5d4ff9f22fdba57cb849d05e380bc9d1f258ce00
Dean <72749793+drossano@users.noreply.github.com>
2022-08-25 20:42:25 -0400

    Switch test to test for valid input first

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,14 @@
 class Player
   def input(array)
-    min = 1
-    max = array.length
-    input = gets.chomp.to_i
-    if input > max
-       puts "Invalid input. Please try again."
+    loop do
+      min = 1
+      max = array.length
+      input = gets.chomp.to_i
+      if input.between?(min, max)
+        return input
+      else 
+        puts "Invalid input. Please enter a number between #{min} and #{max}."
+      end   
     end
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,15 +5,17 @@ describe Player do
   subject(:player_input) { described_class.new }
   let(:board_input) { instance_double(Board)}
   describe '#input' do
-    context 'when the input is not between 1 and 7' do
+    context 'when a valid input is entered' do
       before do
-        invalid_input = "8"
-        allow(player_input).to receive(:gets).and_return(invalid_input)
+        valid_input = '3'
+        allow(player_input).to receive(:gets).and_return(valid_input)
       end
-      it 'returns an error message' do
+      it 'stops the loop and does not display an error message' do
+        error_message = "Invalid input. Please enter a number bewteen 1 and 7."
         allow(board_input).to receive(:board_array).and_return(Array.new(7) { Array.new(6, ' ') })
-        expect(player_input).to receive(:puts).with("Invalid input. Please try again.")
-        player_input.input(board_input.board_array)
+        array = board_input.board_array
+        expect(player_input).not_to receive(:puts).with(error_message)
+        player_input.input(array)
       end
     end
   end
