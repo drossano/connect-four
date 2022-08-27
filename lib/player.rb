b399e90942ef082fd0cd696d52269b00fe380be0
Dean <72749793+drossano@users.noreply.github.com>
2022-08-27 09:31:46 -0400

    Remove input from Player, have class only choose player color

--- lib/player.rb
+++ lib/player.rb
@@ -1,14 +1,5 @@
 class Player
-  def input(array)
-    loop do
-      min = 1
-      max = array.length
-      input = gets.chomp.to_i
-      if input.between?(min, max)
-        return input
-      else 
-        puts "Invalid input. Please enter a number between #{min} and #{max}."
-      end   
-    end
+  def initialize(color)
+    @color = color
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,21 +2,4 @@ require_relative '../lib/player'
 require_relative '../lib/board'
 
 describe Player do
-  subject(:player_input) { described_class.new }
-  let(:board_input) { instance_double(Board)}
-  describe '#input' do
-    context 'when a valid input is entered' do
-      before do
-        valid_input = '3'
-        allow(player_input).to receive(:gets).and_return(valid_input)
-      end
-      it 'stops the loop and does not display an error message' do
-        error_message = "Invalid input. Please enter a number bewteen 1 and 7."
-        allow(board_input).to receive(:board_array).and_return(Array.new(7) { Array.new(6, ' ') })
-        array = board_input.board_array
-        expect(player_input).not_to receive(:puts).with(error_message)
-        player_input.input(array)
-      end
-    end
-  end
 end
